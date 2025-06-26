4c0f174c6b703cac7c62350d1b9698023c764279
LetsLeo <126340239+LetsLeo@users.noreply.github.com>
2025-06-26 22:33:12 +0200

    Add files via upload

---
+++ tabs.js
@@ -0,0 +1,18 @@
+const quizTabBtn = document.getElementById("quiz-tab-btn");
+const gameTabBtn = document.getElementById("game-tab-btn");
+const quizSection = document.getElementById("quiz-section");
+const gameSection = document.getElementById("game-section");
+
+quizTabBtn.onclick = () => {
+  quizTabBtn.classList.add("active");
+  gameTabBtn.classList.remove("active");
+  quizSection.classList.add("active");
+  gameSection.classList.remove("active");
+};
+gameTabBtn.onclick = () => {
+  gameTabBtn.classList.add("active");
+  quizTabBtn.classList.remove("active");
+  gameSection.classList.add("active");
+  quizSection.classList.remove("active");
+  if (typeof setGameModeButtons === "function") setGameModeButtons();
+};
